fe218b72e9fa3322e938e4f2485342c9ddce6189
Kimberley Baldwin <kimberleybaldwin@Kimberleys-MacBook-Air.local>
2022-07-31 14:03:15 -0600

    updates to SLL

--- finalproject/src/main/java/mylib/datastructures/linear/SLL.java
+++ finalproject/src/main/java/mylib/datastructures/linear/SLL.java
@@ -18,9 +18,98 @@ public class SLL
 
     //public void insertionSort() {}
 
-    // public void insertInOrder(linkedListNode node) {
-    //     current = head;
-        
-    // }
+    //public void insertInOrder(linkedListNode node) {}
+
+    public linkedListNode removeEndElement() {
+		current = head;
+		while(current != null) {
+			current = current.getNext();
+			if(current.getNext().getNext() == null){
+				temp = current.getNext();
+				current.setNext(null);
+				return temp;
+			}
+
+		}
+		return temp;
+	}
+
+	public linkedListNode removeFirstElement() {
+		temp = head;
+		head = head.getNext();
+		return temp;
+	}
+		
+	public linkedListNode removeElement(int id) {
+		current = head;
+		//If the id given is the first element
+		if(head.getId() == id) {
+			return removeFirstElement();
+		}
+		while(current != null) {
+			current = current.getNext();
+			//If the id given is the last element
+			if(current.getNext() == null && current.getId() == id) {
+				return removeEndElement();
+			}
+			if(current.getNext().getId() == id) {
+				temp = current.getNext();
+				current.setNext(current.getNext().getNext());
+				return temp;
+			}
+		}
+		return temp;
+	}
+
+	public void insertToEndOfList(linkedListNode node) {
+		
+		if (head == null) {
+			head = node;
+		}
+		else {	
+			getLastNode().setNext(node);
+		}
+		
+	}
+	public void insertToFrontOfList (linkedListNode node) {
+		if (head == null) {
+			head = node;
+		}
+		else {
+			node.setNext(head);
+			head = node;
+		}
+	}
+ 
+	public void recursivePrintList () {
+		recursivePrintList (head);
+	}
+	private void recursivePrintList (linkedListNode cursor) {
+		
+		if (cursor != null) {
+			
+			System.out.println(cursor);
+			recursivePrintList (cursor.getNext());
+		}
+	}
+	public void printList () {
+		Student cursor = head;
+		while (cursor != null) {
+			System.out.println(cursor);
+			cursor = cursor.getNext();
+		}
+		
+	}
+	private linkedListNode getLastNode() {
+		linkedListNode cursor = head;
+		while (cursor.getNext() != null) {
+			cursor = cursor.getNext();
+		}
+		return cursor;
+	}
+	public linkedListNode getHead() {
+		return head;
+	}
+
 
 }
--- finalproject/src/main/java/mylib/datastructures/nodes/linkedListNode.java
+++ finalproject/src/main/java/mylib/datastructures/nodes/linkedListNode.java
@@ -4,10 +4,12 @@ package mylib;
 public class linkedListNode
 {
     private linkedListNode next;
+    private int data;
     
     //Constructor
-    public linkedListNode(linkedListNode next) {
-        this.next = next;
+    public linkedListNode(int data) {
+        setNext(null);
+        setData(data);
     }
 
     public linkedListNode getNext() {
@@ -17,4 +19,17 @@ public class linkedListNode
     public void setNext(linkedListNode next) {
         this.next = next;
     }   
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Value:" + getData();
+    }
 }
