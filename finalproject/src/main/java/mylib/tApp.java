ddf59bbc42a9ca7f9f377c8bc794ecf18978c185
Tyson Trail <tyson.trail@gmail.com>
2022-08-02 10:20:50 -0600

    Commit

--- finalproject/src/main/java/mylib/tApp.java
+++ finalproject/src/main/java/mylib/tApp.java
@@ -1,13 +1,5 @@
 package mylib;
 
-/**
- * Hello world!
- *
- */
-public class tApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class tApp {
+  public static void main(String[] args) {}
 }
